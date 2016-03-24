c2b6fa1f00e2c2bc8cb3c6c977f36acf63a707b6
Lee Foster <lee@leefoster.me>
2016-03-23 21:40:25 -0700

    Changed background overlay and whitespace in post list

--- www/app/directives/backImg.directive.js
+++ www/app/directives/backImg.directive.js
@@ -3,8 +3,9 @@ angular.module("snoosnoo").directive('backImg', function(){
         var imgURL = attrs.backImg;
         if (imgURL) {
             element.css({
-                'background': "linear-gradient(-180deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.80) 100%), url(" + imgURL +') no-repeat',
-                'background-size': 'auto auto',
+                'background': "linear-gradient(-180deg, rgba(0,0,0,0.35) 100%, rgba(0,0,0,0.30) 100%), url(" + imgURL +') no-repeat',
+                // 'background': "linear-gradient(-180deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.80) 100%), url(" + imgURL +') no-repeat',
+                'background-size': '100% auto',
                 'background-position': 'center'
             });
         }
